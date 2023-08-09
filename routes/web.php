3e5966714d8f14496eb50317003f8e231cf70857
Abraham <athioii6@gmail.com>
2023-08-09 18:34:22 +0700

    adding auth with ui bootstrap, configured auth to admin

--- routes/web.php
+++ routes/web.php
@@ -5,39 +5,43 @@ use App\Http\Controllers\BrandController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/contacts', function() {
-    return view('contacts');
-})->name('contacts.index');
-
+Route::middleware(['guest'])->group(function() {
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/contacts', function() {
+        return view('contacts');
+    })->name('contacts.index');
+});
 
 Route::prefix('/admin')->group(function(){
-    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
-    Route::prefix('/products')->group(function(){
-        Route::controller(ProductController::class)->group(function(){
-             Route::get('/',[AdminController::class, 'index'])->name('index.products');
-             Route::get('/create','create')->name('create.product.index');
-             Route::post('/create','store')->name('create.product');
-             Route::get('{id}/show','show')->name('show.product');
-             Route::get('/{id}/edit','edit')->name('edit.product');
-             Route::put('/{id}','update')->name('update.product');
-             Route::delete('{id}/delete','destroy')->name('delete.product');
+    Auth::routes();
+    Route::middleware(['auth'])->group(function() {
+        Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+        Route::prefix('/products')->group(function(){
+            Route::controller(ProductController::class)->group(function(){
+                 Route::get('/',[AdminController::class, 'index'])->name('index.products');
+                 Route::get('/create','create')->name('create.product.index');
+                 Route::post('/create','store')->name('create.product');
+                 Route::get('{id}/show','show')->name('show.product');
+                 Route::get('/{id}/edit','edit')->name('edit.product');
+                 Route::put('/{id}','update')->name('update.product');
+                 Route::delete('{id}/delete','destroy')->name('delete.product');
+            });
         });
-    });
-
-    Route::prefix('/brands')->group(function(){
-        Route::controller(BrandController::class)->group(function(){
-            Route::get('/','index')->name('index.brands');
-            Route::get('/create','create')->name('create.brand.index');
-            Route::post('/create','store')->name('create.brand');
-            Route::get('{id}/show','show')->name('show.brand');
-            Route::get('/{id}/edit','edit')->name('edit.brand');
-            Route::put('/{id}','update')->name('update.brand');
-            Route::delete('{id}/delete','destroy')->name('delete.brand');
+    
+        Route::prefix('/brands')->group(function(){
+            Route::controller(BrandController::class)->group(function(){
+                Route::get('/','index')->name('index.brands');
+                Route::get('/create','create')->name('create.brand.index');
+                Route::post('/create','store')->name('create.brand');
+                Route::get('{id}/show','show')->name('show.brand');
+                Route::get('/{id}/edit','edit')->name('edit.brand');
+                Route::put('/{id}','update')->name('update.brand');
+                Route::delete('{id}/delete','destroy')->name('delete.brand');
+            });
         });
     });
 
@@ -58,4 +62,7 @@ Route::prefix('/admin')->group(function(){
 //     Route::controller(BrandController::class)->group(function() {
 //         Route::get('/brands', 'index');
 //     });
+
 // });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
