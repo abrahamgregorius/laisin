16febca566bcca10eb04dc67312f6c041a2f864d
Abraham <athioii6@gmail.com>
2023-08-09 17:35:30 +0700

    changes on route and hrefs

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -18,10 +18,10 @@
             </div>
             <ul class="laisin-menu">
                 <li class="@if(request()->routeIs('home.index')) laisin-active @endif"><a href="{{ route('home.index') }}">Home</a></li>
-                <li class="@if(request()->routeIs('products.show')) laisin-active @endif"><a href="{{ route('products.show') }}">Products</a></li>
+                <li class="@if(request()->routeIs('products.index')) laisin-active @endif"><a href="{{ route('products.index') }}">Products</a></li>
                 <li class="@if(request()->routeIs('contacts.index')) laisin-active @endif"><a href={{ route('contacts.index') }}>Contact</a></li>
-                <li class="@if(request()->routeIs('products.show')) {{-- laisin-active --}} @endif">
-                    <a href={{ route('products.show') }}>
+                <li class="@if(request()->routeIs('products.index')) {{-- laisin-active --}} @endif">
+                    <a href={{ route('products.index') }}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                     </svg>
--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,17 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
-<<<<<<< Updated upstream
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/contacts', function() {
     return view('contacts');
 })->name('contacts.index');
 
 
-// TODO: route dibawah diexecute setelah Login, buat middleware
 Route::prefix('/admin')->group(function(){
-
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
-    
     Route::prefix('/products')->group(function(){
         Route::controller(ProductController::class)->group(function(){
-             Route::get('/','admin_index')->name('index.products');
+             Route::get('/',[AdminController::class, 'index'])->name('index.products');
              Route::get('/create','create')->name('create.product.index');
              Route::post('/create','store')->name('create.product');
              Route::get('{id}/show','show')->name('show.product');
@@ -48,30 +44,18 @@ Route::prefix('/admin')->group(function(){
 });
 
 
-=======
-Route::get('/products', function() {return view('products');})->name('products.show');
-Route::get('/contacts', function() {return view('contacts');})->name('contacts.index');
-Route::get('/admin', [AdminController::class, 'index']);
->>>>>>> Stashed changes
 
-Route::middleware('isLogin')->group(function() {
-    Route::prefix('/admin')->group(function() {
-        Route::controller(ProductController::class)->group(function (){
-            Route::get('/products', 'index')->name('products.index');
-            Route::post('/products/create', 'store');
-            Route::put('/products/{id}', 'update');
-            Route::delete('/products/{id}/delete', 'destroy');
-        });
-    });
+// Route::middleware('isLogin')->group(function() {
+//     Route::prefix('/admin')->group(function() {
+//         Route::controller(ProductController::class)->group(function (){
+//             Route::get('/products', 'index')->name('products.index');
+//             Route::post('/products/create', 'store');
+//             Route::put('/products/{id}', 'update');
+//             Route::delete('/products/{id}/delete', 'destroy');
+//         });
+//     });
     
-    Route::controller(BrandController::class)->group(function() {
-        Route::get('/brands', 'index');
-    });
-});
-
-<<<<<<< Updated upstream
-Route::controller(BrandController::class)->group(function() {
-    Route::get('/brands', 'index');
-});
-=======
->>>>>>> Stashed changes
+//     Route::controller(BrandController::class)->group(function() {
+//         Route::get('/brands', 'index');
+//     });
+// });
