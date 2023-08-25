f90f92f7a1bc888792c5c906f90a74db9eef6180
ikhsanadrians <aikhsan032@gmail.com>
2023-08-25 08:58:04 +0700

    update animation in product show

--- public/laisin-js/laisin-product.js
+++ public/laisin-js/laisin-product.js
@@ -5,12 +5,8 @@ $('.laisin-show-product-menu').children().on('click', function() {
         $('.laisin-show-product-menu').children().removeClass('menu-active');
         $(this).addClass('menu-active');
 
-        if (currentPosition == 1) {
-            $('.product-spesification, .product-description').toggleClass('hidden');
-            currentPosition = 2;
-        } else {
-            $('.product-spesification, .product-description').toggleClass('hidden');
-            currentPosition = 1;
-        }
+        $('.product-spesification, .product-description').slideToggle('slow'); // Using slideToggle for animation
+        
+        currentPosition = (currentPosition === 1) ? 2 : 1; // Ternary operator for concise code
     }
 });
