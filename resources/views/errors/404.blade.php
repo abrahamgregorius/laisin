978033c7e77396f67c643a8a5cb5beb4f81c70b3
ikhsanadrians <aikhsan032@gmail.com>
2023-08-25 20:52:43 +0700

    update 404 page add vector

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
 <div class="not-found">
+    <img src="{{ asset('assets/elements/404 Error-pana.svg')}}" alt="" height="250px">
     <p>404 - Not Found</p>
     <a href="/">Click here to go home!</a>
 </div>
