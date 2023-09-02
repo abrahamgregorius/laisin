db5cb3a5f25e97f9aa381f593caf1135b8bfde15
Abraham <athioii6@gmail.com>
2023-09-03 01:56:18 +0700

    add phone number and links to footer

--- resources/views/layout/components/footer.blade.php
+++ resources/views/layout/components/footer.blade.php
@@ -10,17 +10,50 @@
             <div class="footer-menus col-md-8">
                 <ul class="menu-1">
                      <li class="menu-head">LINKS<li>
-                     <li class="menu-link">Home</li>
-                     <li class="menu-link">Products</li>
-                     <li class="menu-link">Contact</li>
-                     <li class="menu-link">Find A Parts</li>
+                     <li class="menu-link">
+                        <a href="/">
+                            Home
+                        </a>
+                    </li>
+                     <li class="menu-link">
+                        <a href="/products">
+                            Products
+                        </a>
+                    </li>
+                     <li class="menu-link">
+                        <a href="/contacts">
+                            Contact
+                        </a>
+                    </li>
+                     <li class="menu-link">
+                        <a href="/products">
+                            Find A Part
+                        </a>
+                    </li>
                 </ul>
+
                 <ul class="menu-2">
                    <li class="menu-head">PRODUCTS</li>
-                   <li class="menu-link">Toyota</li>
-                   <li class="menu-link">Ford</li>
-                   <li class="menu-link">Scania</li>
-                   <li class="menu-link">Mitsubishi</li>
+                   <li class="menu-link">
+                    <a href="/products/brand/toyota">
+                        Toyota
+                    </a>
+                </li>
+                 <li class="menu-link">
+                    <a href="/products/brand/chevrolet">
+                        Chevrolet
+                    </a>
+                </li>
+                 <li class="menu-link">
+                    <a href="/products/brand/nissan">
+                        Nissan
+                    </a>
+                </li>
+                 <li class="menu-link">
+                    <a href="/products/brand/honda">
+                        Honda
+                    </a>
+                </li>
                 </ul>
                 <ul class="menu-3">
                   <li class="menu-head">OFFICE</li>
@@ -28,10 +61,13 @@
                     PT. LAISINDO ANUGERAH JAYAABADI
                   </li>
                   <li class="address">
+                    +62 812-8781-1741
+                    <br>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                       </svg>
-                    JL. RAYA PERANCIS KOMP. PERGUD 75 BLOK J NO 3 AB, RT.004/RW.008, Benda, Kec. Benda, Kota Tangerang, Banten 15125</li>
+                    JL. RAYA PERANCIS KOMP. PERGUD 75 BLOK J NO 3 AB, RT.004/RW.008, Benda, Kec. Benda, Kota Tangerang, Banten 15125
+                </li>                    
                 </ul>
             </div>
         </div>
