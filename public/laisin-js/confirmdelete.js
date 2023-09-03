de4473ba8c98f3f9442f0531545be69728865fd4
ikhsanadrians <aikhsan032@gmail.com>
2023-09-03 18:00:30 +0700

    add category confirm delete

--- public/laisin-js/confirmdelete.js
+++ public/laisin-js/confirmdelete.js
@@ -64,3 +64,24 @@ $('.brand-confirm-delete').on('click', function(e) {
         }
     })
 })
+
+$('.category-confirm-delete').on('click', function(e) {
+    e.preventDefault()
+    var form = $(this).closest("form");
+    var name = $(this).data("name");
+    swal.fire({
+        title: `Are you sure you want to delete this Response?`,
+        text: "If you delete this, it will Permanently Deleted.",
+        type: "warning",
+        showCancelButton: !0,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        // reverseButtons: 0,
+    }).then(function(e) {
+        if (e.value === true) {
+            form.submit();
+        } else {
+            e.dismiss;
+        }
+    })
+})
