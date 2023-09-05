32b9c9d80a62f87324d6d8bd15b497cf32aef9d3
Abraham <athioii6@gmail.com>
2023-09-05 08:41:26 +0700

    try to fix hamburger

--- resources/js/laisin-js/hamburger.js
+++ resources/js/laisin-js/hamburger.js
@@ -1,15 +1,26 @@
-const hamburger = document.querySelector(".hamburger"),
-    mobileNav = document.querySelector(".mobile-nav"),
-    hamburgerToggle = document.querySelectorAll(".hamburger-toggle"),
-    closeMobileNav = document.querySelector(".close-mobile-nav"),
-    toggleMobileNav = () => { mobileNav.classList.toggle("slide"), hamburgerToggle.forEach(e => e.classList.toggle("hide")) };
-    hamburger.addEventListener("click", () => {
-        toggleMobileNav()
-        console.log('hamburger clicked')
-    })
-    closeMobileNav.addEventListener("click", () => {
-        toggleMobileNav()
-        console.log('hamburger clicked')
-    });
+const hamburger = document.querySelector(".hamburger")
+const mobileNav = document.querySelector(".mobile-nav")
+const hamburgerToggle = document.querySelectorAll(".hamburger-toggle")
+const closeMobileNav = document.querySelector(".close-mobile-nav")
+const toggleMobileNav = () => {
+    mobileNav.classList.toggle("slide") 
+    hamburgerToggle.forEach(e => e.classList.toggle("hide")) 
+};
+    
+hamburger.addEventListener("click", () => {
+    console.log(mobileNav.classList.contains('slide'))
+    if(!mobileNav.classList.contains('slide')){
+        mobileNav.classList.add("slide")
+    }else {
+        mobileNav.classList.remove("slide")        
+    }
+    console.log('hamburger clicked')
+})
+
+closeMobileNav.addEventListener("click", () => {
+    mobileNav.classList.remove("slide") 
+    console.log('hamburger clicked')
+});
+
 console.log('hamburger loaded')
 
